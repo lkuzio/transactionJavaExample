895792f90deadbd63dcda8b248597eaacea70760
Lukasz Kuzio (lkuzio) <lkuzio@pgs-soft.com>
2015-04-13 10:06:32 +0200

    db structure fix

--- transactionExample/src/resources/db/dbinit.sql
+++ transactionExample/src/resources/db/dbinit.sql
@@ -4,12 +4,12 @@ CREATE TABLE users
   name character varying(255) NOT NULL,
   password character varying(255) NOT NULL,
   group_id bigint,
-  CONSTRAINT users_pkey PRIMARY KEY (id),
+  CONSTRAINT users_pkey PRIMARY KEY (id)
 );
 
 CREATE TABLE groups
 (
   id bigint NOT NULL,
   name character varying(255) NOT NULL,
-  CONSTRAINT users_pkey PRIMARY KEY (id),
+  CONSTRAINT groups_pkey PRIMARY KEY (id)
 );
